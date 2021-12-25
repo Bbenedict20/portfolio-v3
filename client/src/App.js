555eae64856dca0b33787c9b3a4cc09e43c61452
Bbenedict20 <Bradbenedict200@gmail.com>
2021-12-24 17:21:44 -0700

    updated forms

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import Nav from './Nav';
 function App() {
 
   window.onscroll = () => {
-    const navBar = document.querySelector('.navi');
+    if (window.innerWidth > 840) {
+      const navBar = document.querySelector('.navi');
 
-    if (window.scrollY > 80) {
-      navBar.classList.add('nav-bg');
-    } else {
-      navBar.classList.remove('nav-bg');
+      if (window.scrollY > 80) {
+        navBar.classList.add('nav-bg');
+      } else {
+        navBar.classList.remove('nav-bg');
+      }
     }
   }
 
--- client/src/Contact.js
+++ client/src/Contact.js
@@ -9,53 +9,76 @@ const Contact = () => {
         email: '',
         message: ''
     })
+    const [showText, setShowText] = useState(false);
     const [status, setStatus] = useState('notSent');
 
     const handleChange = e => {
         setData({ ...data, [e.target.name]: e.target.value })
     }
     const handleSubmit = async e => {
+        const { name, phone, email, message } = data;
         e.preventDefault();
-        await axios.post('./contact', data)
-            .then(setStatus('sending'))
-            .then(res => {
-                if (res.data === 'success') {
-                    setStatus('sent')
+        if (name.length > 0 && phone.length > 0 & email.length > 0 && message.length > 0) {
+            const form = Array.from(document.querySelector('#inputs').children)
+
+            setShowText(false);
+            for (let item of form) {
+                item.classList.remove('missing');
+            }
+            await axios.post('./contact', data)
+                .then(setStatus('sending'))
+                .then(res => {
+                    if (res.data === 'success') {
+                        setStatus('sent')
+                    } else {
+                        setStatus('failed')
+                        alert('Error sending message.')
+                    }
+                })
+        } else {
+            const form = Array.from(document.querySelector('#inputs').children)
+            for (let item of form) {
+                if (item.value === '') {
+                    item.classList.add('missing')
+                    setShowText(true);
                 } else {
-                    setStatus('failed')
-                    alert('Error sending message.')
+                    item.classList.remove('missing')
                 }
-            })
+            }
+        }
     }
     return (
         <form onSubmit={handleSubmit} className="flex col ai-c">
-            <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                value={data.name}
-                onChange={handleChange}
-            />
-            <input
-                type="tel"
-                name="phone"
-                placeholder="Phone Number"
-                value={data.phone}
-                onChange={handleChange}
-            />
-            <input
-                type="email"
-                name="email"
-                placeholder="E-mail"
-                value={data.email}
-                onChange={handleChange}
-            />
-            <textarea
-                name="message"
-                value={data.message}
-                onChange={handleChange}
-                placeholder="Your Message"
-            ></textarea>
+            <p className={showText ? 'missing-text show-text' : 'missing-text'}>Please fill out missing information</p>
+            <div id="inputs">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={data.name}
+                    onChange={handleChange}
+                />
+                <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone Number"
+                    value={data.phone}
+                    onChange={handleChange}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="E-mail"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+                <textarea
+                    name="message"
+                    value={data.message}
+                    onChange={handleChange}
+                    placeholder="Your Message"
+                ></textarea>
+            </div>
             {status === 'sending' ? <h2>Sending...</h2> : status === 'sent' ? <h2>Message sent!</h2> : <button>Send</button>}
         </form>
     )
